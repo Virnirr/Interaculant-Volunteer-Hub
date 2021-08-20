139d98453dcbcec567e71ca0028c3152405177d7
Virnir <zhihe122@gmail.com>
2021-08-19 22:58:09 -0700

    Made Navbar

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask!"
