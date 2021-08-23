1e00c8dda92d9217e7858b4286f7beb18c01ebf0
Virnir <zhihe122@gmail.com>
2021-08-22 23:00:25 -0700

    Completed my landing and contact front end.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import os
-
+import sqlite3
+from sqlite3.dbapi2 import Timestamp
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
@@ -7,6 +7,7 @@ from werkzeug.exceptions import default_exceptions, HTTPException, InternalServe
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
+from helper import dict_factory
 # My app
 app = Flask(__name__)
 
@@ -29,14 +30,12 @@ Session(app)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template("landing.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
 
-    # Forget any user_id
-    session.clear()
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
         email = request.form.get("email")
@@ -44,26 +43,55 @@ def login():
         
         # Query database for username
         con = sqlite3.connect("volunteer.db")
+        con.row_factory = dict_factory
         db = con.cursor()
-        rows = db.execute("SELECT * FROM users WHERE = :email", email=email)
+
+        rows = db.execute("SELECT * FROM users WHERE email=?", [email]).fetchall()
 
         # Ensure that email and password exist
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return redirect("/")
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
+            con.close()
+            flash("Invalid Email and/or Password", "danger")
+            return redirect("/login")
         
         con.commit()
         con.close()
 
+        # Forget any user_id
+        session.clear()
+
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        flash("Login Successfully!", "info")
+        return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
 
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        return redirect("/contact")
+
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("contact.html")
+    
+
 @app.route("/logout")
 def logout():
     """Log user out"""
+
+    # Flash message for logout
+    if "user_id" in session:
+        user = session["user_id"]
+        flash(f"You have been logged out, {user}", "info")
 
     # Forget any user_id
     session.clear()
@@ -80,35 +108,36 @@ def register():
         username = request.form.get("username")
         email = request.form.get("email")
         password = request.form.get("password")
-        confirmation = request.form.get("confirmation")
-        
-        if not password or password != confirmation:
-            return render_template("password_mistake_html")
         
         # Logs user into database
         con = sqlite3.connect("volunteer.db")
         db = con.cursor()
-        rows = db.execute("INSERT INTO users(email, username, hash) VALUES(?, ?, ?)", 
-                          email, username, generate_password_hash( password, method='pbkdf2:sha256', salt_length=8))
+        
+        rows = db.execute("SELECT * FROM users WHERE username = ?", [username]).fetchall()
+        email_check = db.execute("SELECT * FROM users WHERE email = ?", [email]).fetchall()
+
+        if len(rows) != 0:
+            con.close()
+            flash("Username Already Taken!", "danger")
+            return redirect("/register")
+        
+        if len(email_check) != 0:
+            con.close()
+            flash("Email Already Taken!", "danger")
+            return redirect("/register")
+
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+        db.execute("INSERT INTO users(email, username, hash) VALUES(?, ?, ?)", 
+                          [email, username, hashed_password])
         
         con.commit()
         con.close()
 
         # Reddirect user back to login page after registering
-        return redirect("login.html")
+        
+        flash("Register Successfully!", "success")
+        return redirect("/login")
+
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("register.html")
-
-
-# Error Handler
-def errorhandler(e):
-    """Handle error"""
-    if not isinstance(e, HTTPException):
-        e = InternalServerError()
-    return render_template("error.html")
-
-
-# Listen for errors
-for code in default_exceptions:
-    app.errorhandler(code)(errorhandler)
