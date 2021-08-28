38cc613e9a9b852ad349774c146c5d87a96d93dc
Virnir <zhihe122@gmail.com>
2021-08-28 15:52:45 -0700

    Pretty Much Done

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import sqlite3
-from sqlite3.dbapi2 import Timestamp
 from flask import Flask, flash, redirect, render_template, request, session, jsonify
 from flask_session import Session
 from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 from flask_mail import Mail, Message
